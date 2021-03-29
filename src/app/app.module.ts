f49c1fb6a3f986adac699ad77989680d21ba1cc8
akuniyoshi <alejandro.kuniyoshi@gmail.com>
2021-03-28 23:49:25 -0300

    seccion 3 final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PortafolioComponent } from './pages/portafolio/portafolio.component';
-
-
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 @NgModule({
   declarations: [
@@ -13,9 +15,12 @@ import { PortafolioComponent } from './pages/portafolio/portafolio.component';
     HeaderComponent,
     FooterComponent,
     PortafolioComponent,
+    AboutComponent,
+    ItemComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
